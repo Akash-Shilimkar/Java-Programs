1c3752a4d4484971757e0beffe521788be9e56df
Akash <99742784+Akash-Shilimkar@users.noreply.github.com>
2025-11-07 12:06:30 +0530

    Java Program to  Remove Duplicate Character string.
    This program removes duplicate characters from a user-input string. It iterates through each character, checks if it already exists in the result string using indexOf(), and appends it only if it’s not present — effectively keeping only unique characters. Finally, it displays the string without duplicates.

---
+++ string_program/RemoveDuplicateCharacter.java
@@ -0,0 +1,21 @@
+package string_program;
+
+import java.util.*;
+
+public class RemoveDuplicateCharacter{
+  public static void main(String [] args){
+    Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the String: ");
+		String str = sc.nextLine();
+    
+    String result = " ";
+    for(int i = 0;i < str.length();i++){
+      char ch = str.charAt(i);
+      if(result.indexOf(ch) == -1){
+        result += ch;
+      }
+      System.out.println("After removing duplicte"+result);
+    }
+  }
+}
